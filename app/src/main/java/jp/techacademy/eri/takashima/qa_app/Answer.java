8cd247d4a3e70e9e66ecd52b582c500254c9a3ff
Eri Takashima <efuruk@gmail.com>
2016-09-09 00:48:44 -0500

    Initial commit

---
+++ app/src/main/java/jp/techacademy/eri/takashima/qa_app/Answer.java
@@ -0,0 +1,35 @@
+package jp.techacademy.eri.takashima.qa_app;
+
+/**
+ * Created by hajime04 on 9/8/2016.
+ */
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+    public String getUid() {
+        return mUid;
+    }
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+
+
+}
---
+++ app/src/main/java/jp/techacademy/eri/takashima/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.eri.takashima.qa_app;
+
+/**
+ * Created by hajime04 on 9/6/2016.
+ */
+public class Const {
+    public static final String UsersPATH = "users";
+    public static final String ContentsPATH = "contents";
+    public static final String AnswersPATH = "answers";
+
+    public static final String NameKEY = "name";
+
+}
